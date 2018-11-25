00c50bc86829c181330e97db09d8bb3e14f01eef
Jakob <jakobfischer93@gmail.com>
2018-11-25 14:51:45 +0100

    Changed char replacement.

--- word.py
+++ word.py
@@ -3,13 +3,8 @@ import re
 def find_codewords(codeword_fragment, wordlist):
     match_list = wordlist.copy()
     match_list = [word for word in match_list if len(word) >= len(codeword_fragment) and len(set(word)) == len(word)]
-    ex = ""
-    for char in codeword_fragment:
-        if char == "_":
-            ex += "."
-        else:
-            ex += char
-    pattern = re.compile(ex)
+    codeword_regex = codeword_fragment.replace("_", ".")
+    pattern = re.compile(codeword_regex)
     match_list = [word for word in match_list if pattern.search(word) != None]
     return match_list
 
